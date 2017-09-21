acc2c78027cff47b404c37d9af317da8d3935f20
David <david.mougeolle@gmail.com>
2017-09-21 18:43:06 +0200

    modified:   main.py 	modified:   pattern.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pattern
 from PIL import Image
 
-test_image = Image.open('images/monte-rosa.jpg')
+test_image = Image.open('images/firebase.png')
 
 pattern = pattern.Pattern(test_image)
 pattern.preprocess()
--- pattern.py
+++ pattern.py
@@ -1,6 +1,7 @@
 import numpy as np
-from PIL import Image as Pimage
+from PIL import Image
 
+# TODO normaliser / augmenter le contraste : plutot que d'avoir un critere absolu b&w
 # TODO even the sheet spacing : if a 
 
 class Pattern(object):
@@ -22,29 +23,49 @@ class Pattern(object):
         - rec sheet count : {rec_sheet_count}"""
 
     def preprocess(self):
+        self._fill_background()
         self._transform_black_white()
+        self._image.show()
+        self._emphasize()
+        self._image.show()
+        self._transform_binary()
+        self._image.show()
         self._crop()
         self._update_attributes()
-        self._image.show()
 
     def postprocess(self):
         self._smooth_pattern()
         self._update_attributes()
-        self._image.show()
 
     def _update_attributes(self):
         self.width = self._image.width
         self.height = self._image.height
 
+    def _fill_background(self):
+        if self._image.mode == 'RGBA':
+            self._image.load()
+            white_bg_image = Image.new('RGBA', self._image.size, (255,255,255))
+            self._image = white_bg_image.paste(self._image, mask=self._image.split()[3])
+
+    def _emphasize(self):
+        temp = np.array(self._image)
+        min_level = np.amin(temp)
+        max_level = np.amax(temp[temp < 255])
+        if max_level > min_level:
+            self._image = self._image.point(lambda x: 255. * (x - min_level) / (max_level - min_level))
+
     def _transform_black_white(self):
         self._image = self._image.convert("L")
+        
+    def _transform_binary(self):
         self._image = self._image.point(lambda x: 0 if x<85 else 255, '1')
 
     def _crop(self):
         temp = np.invert(np.array(self._image))
         is_image_empty = np.sum(temp)
+        is_image_empty = is_image_empty == 0.0
         
-        if is_image_empty > 0.0:
+        if not is_image_empty:
             x_array = np.sum(temp, axis=1)
             x_array = np.transpose(np.nonzero(x_array))
 
@@ -59,7 +80,7 @@ class Pattern(object):
         else:
             raise Exception('The provided image is empty !')
         
-        self._image.crop((min_x, min_y, max_x, max_y)).show()
+        self._image = self._image.crop((min_y, min_x, max_y, max_x))
 
     def _smooth_pattern(self):
         pass
