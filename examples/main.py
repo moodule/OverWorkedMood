c4936e7ddeee41b960d31abdd3e428167b216fa3
David Mougeolle <david.mougeolle@moodule.net>
2019-07-10 23:13:48 +0200

    Test and showcase the script on monochrome images

--- examples/main.py
+++ examples/main.py
@@ -1,5 +1,5 @@
-import ...overworked.pattern as pattern
-import ...overworked.book as book
+import overworked.pattern as pattern
+import overworked.book as book
 from PIL import Image
 
 # TODO describe the workflow :
@@ -9,19 +9,19 @@ from PIL import Image
 #   fold !
 # actually it can go both ways
 
-pattern = pattern.Pattern('github/github.png')
+pattern = pattern.Pattern('chardon/chardon.jpg')
 pattern.preprocess(invert=False)
 pattern.slice_image()
-pattern.postprocess(160)
+pattern.postprocess(wanted_width=160)
 print pattern
-pattern.save_image_preview(image_name='preview', image_path='github')
+pattern.save_image_preview(image_name='preview', image_path='chardon')
 pattern.show()
 #print pattern._dropout_factor
 
 book = book.Book()
 book.set_size(1, 589, 0.2, 0.15)
 book.set_pattern(pattern)
-book.save_folding_table(pattern_name='pattern', pattern_path='github')
+book.save_folding_table(pattern_name='pattern', pattern_path='chardon')
 print book
 print book.sheet_spacing()
 print book.sheet_count()
