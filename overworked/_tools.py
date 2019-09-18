40b57560fa14214018e307d7ea8de62c87bfe8ae
David Mougeolle <moodule@protonmail.com>
2019-09-18 22:28:02 +0200

    Convert the docstring to numpydoc format, the new std for the project

--- overworked/_tools.py
+++ overworked/_tools.py
@@ -20,28 +20,37 @@ from typical import checks
 
 @checks
 def full_path_to_file(
-        name,
-        path='',
-        extension='',
-        prefix='',
-        suffix=''):
+        name: str,
+        path: str='',
+        extension: str='',
+        prefix: str='',
+        suffix: str=''):
     """Piece together the full path of a file.
 
     The extension and path shouldn't appear in the string argument name.
     If the name string has already an extension, it is kept and the desired extension is appended.
     The '.' are kept in the output name and pushed at the end of the name, after the suffix.
 
-    Args:
-        name (str): the file name as seen in the containing directory
-        path (str): path to the containing directory
-        extension (str): the definitive extension of the output file
-        prefix (str): tag placed before the actual name of the file
-        suffix (str): tag placed after the actual name of the file
+    Parameters
+    ----------
+    name:
+        The file name as seen in the containing directory.
+    path:
+        Path to the containing directory.
+        Can be either relative or absolute.
+    extension:
+        The definitive extension of the output file.
+    prefix:
+        Tag placed before the actual name of the file.
+    suffix:
+        Tag placed after the actual name of the file.
 
-    Returns:
+    Returns
+    -------
         A string containing the full path to the file, in UNIX notation.
     
-    Raises:
+    Raises
+    ------
     """
     file_path, file_name = os.path.split(name)
     file_name, file_extension = os.path.splitext(file_name)
