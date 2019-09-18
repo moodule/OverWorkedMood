46f14be0b3f01a84485018de42bee431e1ac31ef
David Mougeolle <moodule@protonmail.com>
2019-09-18 22:21:35 +0200

    Fill the meta informations of the package

--- overworked/__init__.py
+++ overworked/__init__.py
@@ -1,7 +1,35 @@
 # -*- coding: utf-8 -*-
 
-"""Top-level package for Overworked Moodule."""
+"""
+==========
+Overworked
+==========
 
-__author__ = """David Mougeolle"""
-__email__ = 'david.mougeolle@moodule.net'
-__version__ = '0.0.0'
+A toolbox to plan and visualize the upcycling process of old items. 
+
+Examples
+--------
+    >>> ...
+"""
+
+from __future__ import division, print_function, absolute_import
+
+from overworked.folding import (
+	preprocess_image,
+	slice_image_into_pattern,
+	postprocess_folding_pattern,
+	export_folding_pattern)
+
+__author__ = 'David Mougeolle'
+__email__ = 'moodule@protonmail.com'
+__version__ = '0.6.2'
+__title__ = 'overworked'
+__description__ = 'Generate blueprints to upcycle old items into art.'
+__url__ = 'http://github/moodule/overworked'
+__license__ = 'MIT license'
+
+__all__ = [
+	'preprocess_image',
+	'slice_image_into_folding_pattern',
+	'postprocess_folding_pattern',
+	'export_folding_pattern' ]
