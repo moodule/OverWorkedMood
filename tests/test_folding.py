39797b20125cdf306978c8063cfa5b2cb70a5b9a
David Mougeolle <moodule@protonmail.com>
2019-09-28 16:12:40 +0200

    Test the preprocessing end to end on images

---
+++ tests/test_folding.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `overworked` package."""
+
+import os
+import pytest
+
+from PIL import Image
+
+from overworked import preprocess
+
+#####################################################################
+# _
+#####################################################################
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+
+#####################################################################
+# FIX
+#####################################################################
+
+IMAGE_PATHS = {
+    'binary': [
+        'tests/data/binary/github.png']}
+
+#####################################################################
+# END TO END
+#####################################################################
+
+def test_preprocess_binary_images():
+    for __path in IMAGE_PATHS['binary']:
+        with Image.open(__path) as __image:
+            preprocess(
+                image=__image,
+                invert=False).save(
+                fp=(
+                    '.processed'.join(os.path.splitext(__path))
+                    + '.jpg'),
+                format='JPEG')
